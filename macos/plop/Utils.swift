729dee42ffccffa847bb23b7c9d7a6ccb302d23f
Atul Madhugiri <atulm@berkeley.edu>
2022-05-29 21:12:23 -0400

    [macos] Introduce `generateFileName(nameLength, fileExtension)`
    Summary:
    Generates an `nameLength` length random alphanumeric string suffixed by `fileExtension` (".png" by default)

--- macos/plop/Utils.swift
+++ macos/plop/Utils.swift
@@ -1,8 +1,13 @@
 import Foundation
 import AppKit
 
-func replaceClipboard(with newString: String) {
+func replaceClipboard(with newString: String) -> Void {
   let pasteboard = NSPasteboard.general;
   pasteboard.clearContents()
   pasteboard.writeObjects([newString as NSString])
+}
+
+func generateFileName(nameLength: Int = 6, fileExtension: String = ".png") -> String {
+  let randomString = UUID().uuidString.suffix(6).lowercased()
+  return "\(randomString)\(fileExtension)"
 }
