3b7b57bd200e1cdf46d21c45a24fe23df798de97
Atul Madhugiri <atulm@berkeley.edu>
2022-06-18 19:10:14 -0700

    [macos] Introduce `uploadProgress` and corresponding `ProgressView`

--- macos/plop/BlobPopover.swift
+++ macos/plop/BlobPopover.swift
@@ -1,13 +1,18 @@
 import SwiftUI
 
 struct BlobPopover: View {
+  @State var uploadProgress = 1.0
+
   var body: some View {
     VStack {
       HStack {
         Button(action: {}) { ScreenshotButton() }
         Button(action: {}) { UploadButton() }
       }
-    }
+      if #available(macOS 11.0, *) {
+        ProgressView("", value: uploadProgress, total: 1.0).padding(.horizontal, 20)
+      }
+    }.padding(.all, 20)
   }
 }
 
