d97540de5777569603df9a02ab297125fe68e279
Atul Madhugiri <atulm@berkeley.edu>
2022-06-19 11:25:00 -0700

    [macos] Early-exit `UploadButton` action if `!ModalResponse.OK`

--- macos/plop/UploadButton.swift
+++ macos/plop/UploadButton.swift
@@ -9,19 +9,22 @@ struct UploadButton: View {
       let fileSelectionDialog = NSOpenPanel()
       let fileSelectionResponse = fileSelectionDialog.runModal()
 
-      if fileSelectionResponse == NSApplication.ModalResponse.OK {
-        let (destinationURL, uploadTask) = uploadBlob(
-          filepath: fileSelectionDialog.url!.path)
-
-        previousUploadURL = destinationURL
-        uploadTask.observe(.progress) { snapshot in
-          uploadProgress = snapshot.progress?.fractionCompleted ?? 0
-        }
-        uploadTask.observe(.success) { _ in
-          NSSound(named: "Funk")?.play()
-          replaceClipboard(with: destinationURL)
-        }
+      if fileSelectionResponse != NSApplication.ModalResponse.OK {
+        return
       }
+
+      let (destinationURL, uploadTask) = uploadBlob(
+        filepath: fileSelectionDialog.url!.path)
+
+      previousUploadURL = destinationURL
+      uploadTask.observe(.progress) { snapshot in
+        uploadProgress = snapshot.progress?.fractionCompleted ?? 0
+      }
+      uploadTask.observe(.success) { _ in
+        NSSound(named: "Funk")?.play()
+        replaceClipboard(with: destinationURL)
+      }
+
     }) {
       Text("Upload (")
       Image(systemName: "command")
