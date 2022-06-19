b30dde7a63aac9674e9666853e2c74d2d2a56f31
Atul Madhugiri <atulm@berkeley.edu>
2022-06-19 12:29:54 -0700

    [macos] Replace imperative `NSOpenPanel()` with declarative `fileImporter` in `UploadButton`
    Cleaner, more "idiomatic" approach (I think?) that eliminates the focus issues with `NSOpenPanel()`

--- macos/plop/UploadButton.swift
+++ macos/plop/UploadButton.swift
@@ -4,32 +4,35 @@ struct UploadButton: View {
   @Binding var previousUploadURL: String
   @Binding var uploadProgress: Double
 
+  @State private var fileSelectionDialogPresented = false
+
   var body: some View {
     Button(action: {
-      let fileSelectionDialog = NSOpenPanel()
-      let fileSelectionResponse = fileSelectionDialog.runModal()
-
-      if fileSelectionResponse != NSApplication.ModalResponse.OK {
-        return
-      }
-
-      let (destinationURL, uploadTask) = uploadBlob(
-        filepath: fileSelectionDialog.url!.path)
-
-      previousUploadURL = destinationURL
-      uploadTask.observe(.progress) { snapshot in
-        uploadProgress = snapshot.progress?.fractionCompleted ?? 0
-      }
-      uploadTask.observe(.success) { _ in
-        NSSound(named: "Funk")?.play()
-        replaceClipboard(with: destinationURL)
-      }
-
+      fileSelectionDialogPresented = true
     }) {
       Text("Upload (")
       Image(systemName: "command")
       Image(systemName: "shift")
       Text("U )")
+    }.fileImporter(
+      isPresented: $fileSelectionDialogPresented,
+      allowedContentTypes: [.image, .audio, .video, .text, .data]
+    ) { result in
+      do {
+        let filepath = try result.get().path
+        let (destinationURL, uploadTask) = uploadBlob(filepath: filepath)
+        previousUploadURL = destinationURL
+
+        uploadTask.observe(.progress) { snapshot in
+          uploadProgress = snapshot.progress?.fractionCompleted ?? 0
+        }
+        uploadTask.observe(.success) { _ in
+          NSSound(named: "Funk")?.play()
+          replaceClipboard(with: destinationURL)
+        }
+      } catch {
+        print("Could not get filepath for selected file.")
+      }
     }
   }
 }
