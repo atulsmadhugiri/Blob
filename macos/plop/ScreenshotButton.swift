dcd1f5440272083b0abcc6f4a45cec1c2ce4042d
Atul Madhugiri <atulm@berkeley.edu>
2022-07-09 13:07:29 -0400

    Revert "[macos] Use `VisionKit` to recognize text in screenshots"
    This reverts commit d02e5489cbe34dfadb8f5f6b8e49fe6363dcb85b.

--- macos/plop/ScreenshotButton.swift
+++ macos/plop/ScreenshotButton.swift
@@ -1,25 +1,9 @@
 import SwiftUI
-import Vision
 
 struct ScreenshotButton: View {
   @Binding var previousUploadURL: String
   @Binding var previousUploadLocalPath: URL?
   @Binding var uploadProgress: Double
-  @Binding var recognizedText: String?
-
-  func recognizeTextHandler(request: VNRequest, error: Error?) {
-    guard
-      let observations =
-        request.results as? [VNRecognizedTextObservation]
-    else {
-      return
-    }
-    let recognizedStrings = observations.compactMap { observation in
-      return observation.topCandidates(1).first?.string
-    }
-
-    self.recognizedText = recognizedStrings.joined(separator: " ")
-  }
 
   var body: some View {
     Button(action: {
@@ -34,15 +18,6 @@ struct ScreenshotButton: View {
         replaceClipboard(with: destinationURL)
         previousUploadURL = destinationURL
         previousUploadLocalPath = localPath
-        let nsImage = NSImage(contentsOf: previousUploadLocalPath!)
-        let cgImage = nsImage!.cgImage(forProposedRect: nil, context: nil, hints: nil)!
-        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
-        let request = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
-        do {
-          try requestHandler.perform([request])
-        } catch {
-          print("Unable to perform the requests: \(error).")
-        }
       }
     }) {
       Text("Screenshot (")
@@ -57,6 +32,6 @@ struct ScreenshotButton_Previews: PreviewProvider {
   static var previews: some View {
     ScreenshotButton(
       previousUploadURL: .constant("https://google.com"), previousUploadLocalPath: .constant(nil),
-      uploadProgress: .constant(1.0), recognizedText: .constant("Placeholder"))
+      uploadProgress: .constant(1.0))
   }
 }
