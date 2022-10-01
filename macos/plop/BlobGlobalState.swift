902842b6c42af9fc7559a9ac02099b48b90ed2a0
Atul Madhugiri <atulm@berkeley.edu>
2022-10-01 19:33:04 -0400

    [macos] Remove `anonymousUploadsEnabled: boolean` from `BlobEntry`
    We can just remove this altogether.

--- macos/plop/BlobGlobalState.swift
+++ macos/plop/BlobGlobalState.swift
@@ -4,7 +4,6 @@ class BlobEntry: ObservableObject {
   @Published var uploadProgress: Double = 1.0
   @Published var uploadURL: String = ""
   @Published var uploadLocalPath: URL?
-  @Published var anonymousUploadsEnabled: Bool = false
 }
 
 class BlobGlobalState: ObservableObject {
