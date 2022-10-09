b6d519e9a8958cf81ecaed101c3f29cb0870bb73
Atul Madhugiri <atulm@berkeley.edu>
2022-10-09 19:29:34 -0400

    [macos] Introduce `BlobListCell`

---
+++ macos/plop/BlobListCell.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct BlobListCell: View {
+  var imagePath: URL?
+  var filename: String
+  var filesize: String
+  var fileMIME: String
+
+  var body: some View {
+    HStack {
+      AsyncImage(url: imagePath) { image in
+        image.resizable().scaledToFit().cornerRadius(8).frame(width: 48, height: 48)
+      } placeholder: {
+        Color.red.opacity(0.1).cornerRadius(8).frame(width: 48, height: 48)
+      }
+      VStack(alignment: .leading) {
+        Text(filename).font(.headline)
+        HStack {
+          Text(fileMIME).font(.caption2.monospaced())
+          Text(filesize).font(.caption2.monospaced()).foregroundColor(.gray)
+        }
+      }
+    }
+  }
+}
+
+struct BlobListCell_Previews: PreviewProvider {
+  static var previews: some View {
+    BlobListCell(
+      imagePath: URL(string: "https://blob.sh/atul.png"), filename: "filename.extension",
+      filesize: "[432kb]", fileMIME: "application/octet-stream")
+  }
+}
